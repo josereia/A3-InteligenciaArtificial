74dbb85432b17286c825ac0a5a3db451833f164f
Guilherme <guilherme09112001@gmail.com>
2021-11-19 21:08:22 -0300

    teste

---
+++ card.py
@@ -0,0 +1,15 @@
+# Aqui é onde será a classe de carta
+
+colorsCards = ["yellow", "red", "blue", "green"]
+# Aqui são as cartas comuns
+#key
+#number
+#color
+#value 1
+
+# voltar
+
+#key
+#number
+#color
+#value 2
--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
+import card 
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+baralho = []
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
